1b8b241d54874a574d598ecbe79cdd2afe7d3823
bootjp <oh@bootjp.me>
2017-05-25 16:20:11 +0900

    WIP

--- js/shizuku_core.js
+++ js/shizuku_core.js
@@ -1,9 +1,9 @@
 /* shizuku v0.0.1 https://github.com/bootjp/shizuku (c) bootjp https://bootjp.me/ | */
 var Shizuku = (function() {
-  var _shizuku;
+  var self;
 
   var Shizuku = function(config) {
-    _shizuku = this;
+    self = this;
     if (typeof this.config !== 'undefined') {
       // defined
       return new Shizuku(config);
@@ -41,26 +41,26 @@ var Shizuku = (function() {
   };
 
   Shizuku.prototype.smartphoneOverlayEffect = function() {
-    var container = document.getElementById(_shizuku.config.smartphoneOverlay.targetElementId);
+    var container = document.getElementById(self.config.smartphoneOverlay.targetElementId);
 
     // clear last dom change event and animation
-    clearTimeout(_shizuku.config.smartphoneOverlay.timer);
-    clearInterval(_shizuku.config.smartphoneOverlay.animationTimer);
+    clearTimeout(self.config.smartphoneOverlay.timer);
+    clearInterval(self.config.smartphoneOverlay.animationTimer);
 
-    _shizuku.setStyle(container, {
+    self.setStyle(container, {
       display: 'none',
       opacity: 0
     });
 
-    _shizuku.config.smartphoneOverlay.timer = setTimeout(function() {
+    self.config.smartphoneOverlay.timer = setTimeout(function() {
       var scrollWidth = (document.documentElement.scrollWidth || document.body.scrollWidth);
       var windowShort = Math.min(window.innerWidth, window.innerHeight);
       var windowMargin = (window.innerWidth / scrollWidth);
       var expandWidth = Math.min(scrollWidth * windowMargin, windowShort);
-      var zoomScale = (expandWidth / _shizuku.config.smartphoneOverlay.width);
+      var zoomScale = (expandWidth / self.config.smartphoneOverlay.width);
       var transform = 'scale(' + zoomScale + ', ' + zoomScale + ')';
       var transformOrigin = '0 0';
-      _shizuku.setStyle(container, {
+      self.setStyle(container, {
         display: 'block',
         position: 'absolute',
         transform: transform,
@@ -72,28 +72,28 @@ var Shizuku = (function() {
         zIndex: 0x7FFFFFFF
       });
       var containerRect = container.getBoundingClientRect();
-      _shizuku.setStyle(container, {
+      self.setStyle(container, {
         left: ((window.innerWidth / 2) - (containerRect.width / 2) + window.scrollX) + 'px',
         top: (window.scrollY + window.innerHeight - containerRect.height) + 'px'
       });
 
       var opacity = 0;
       // last animation event clear.
-      _shizuku.config.smartphoneOverlay.animationTimer = setInterval(function() {
-        _shizuku.setStyle(container, {
+      self.config.smartphoneOverlay.animationTimer = setInterval(function() {
+        self.setStyle(container, {
           opacity: ((++opacity) / 100),
           filter: 'alpha(opacity=' + opacity + ')'
         });
 
         if (opacity === 100) {
-          clearInterval(_shizuku.config.smartphoneOverlay.animationTimer);
+          clearInterval(self.config.smartphoneOverlay.animationTimer);
         }
       }, 10);
     }, 300);
 
-    if (!_shizuku.config.smartphoneOverlay.registerEvent) {
-      _shizuku.config.smartphoneOverlay.registerEvent = true;
-      _shizuku.eventRegister([
+    if (!self.config.smartphoneOverlay.registerEvent) {
+      self.config.smartphoneOverlay.registerEvent = true;
+      self.eventRegister([
         'DOMContentLoaded',
         'scroll',
         'touchmove',
